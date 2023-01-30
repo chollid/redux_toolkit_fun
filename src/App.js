f80567e4028af7004bdd8d9e6edd6a295e1b0077
Chris Holliday <chrisuga@me.com>
2023-01-30 15:57:37 -0500

    add asyncthunk's, extraReducers, dynamic items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 
-const App = () => {
+import { useDispatch, useSelector } from "react-redux";
+import { getTotals, getCartItems } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+
+function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    // parameter will go into the async thunk App.js line 15
+    dispatch(getCartItems("random"));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
   );
-};
-
+}
 export default App;
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,30 +1,88 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartitems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { openModal } from "../modal/modalSlice";
+
+const url = "https://course-api.com/react-useReducer-cart-projec";
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 4,
   total: 0,
-  isLoading: true,
+  isLoading: false,
 };
+
+// 1st parameter is name of the action
+// can call getCartItems() in a useEffect to grab the api data - see App.js
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  // name is passed from the cartItems dispatch in App.js
+  // example: you could use this to pass in a user id
+  // if the async thunk requested data for a specific user
+  async (name, thunkAPI) => {
+    try {
+      // So cool: you can access the state of the entire app with
+      // thunkAPI.getState()
+      // Amazing: you can dispatch actions from within an async thunk
+      // open modal from cartSlice.js
+      //   thunkAPI.dispatch(openModal());
+      const response = await axios.get(url);
+
+      return response.data;
+    } catch (err) {
+      // this error is caught in the extraReducers
+      // in .rejected's action.payload
+      return thunkAPI.rejectWithValue({ error: err.message });
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
     clearCart: (state) => {
-      //   state.cartItems = [];
-      return {
-        amount: 4,
-      };
+      state.cartItems = [];
     },
     removeItem: (state, action) => {
       const itemId = action.payload;
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
     },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload);
+      cartItem.amount += 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItems = state.cartItems.find((item) => item.id === payload);
+      cartItems.amount -= 1;
+    },
+    getTotals: (state, { payload }) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      //   console.log("action.payload: ", action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state, action) => {
+      console.log("error action", action);
+      state.isLoading = false;
+    },
   },
 });
 
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, getTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
