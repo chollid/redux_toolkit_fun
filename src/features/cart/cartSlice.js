59c1e022be6d285fd95cafe186f965d76b4de833
Chris Holliday <chrisuga@me.com>
2023-01-29 20:52:17 -0500

    Actions setup for clear cart, remove item

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,8 +11,20 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      //   state.cartItems = [];
+      return {
+        amount: 4,
+      };
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+  },
 });
 
-console.log(cartSlice);
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
