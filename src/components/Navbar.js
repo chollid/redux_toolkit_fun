f64e9fae5e444063c45c41ec56a8215991018081
Chris Holliday <chrisuga@me.com>
2023-02-06 17:22:13 -0500

    Comments navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,13 @@ import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  // useSelector is a hook that allows us to access the store
+  // it takes a callback function as a parameter
+  // the callback function receives the store as a parameter
+  // we can then return the part of the store we want to use
+  // in this case we want to use the cart part of the store
+  // we can then destructure the amount property from the cart part of the store
+  // we can then use the amount property in our component
   const { amount } = useSelector((store) => {
     return store.cart;
   });
